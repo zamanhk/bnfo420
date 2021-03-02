69201b843afc1b6a2c3d26cd9a0050f1e8d49c34
zamanhk <54368676+zamanhk@users.noreply.github.com>
2021-03-02 14:50:08 -0500

    Add files via upload

---
+++ SequenceEncoding.py
@@ -0,0 +1,46 @@
+def encoding(sequence):
+    encoded = []
+    for i in sequence: # turns base pairs into numbers
+        if i == 'A':
+            encoded.append('1')
+        if i == 'T':
+            encoded.append('2')
+        if i == 'G':
+            encoded.append('3')
+        if i == 'C':
+            encoded.append('4')    
+
+    # print encoded prints fine
+
+    seq = []
+
+    for i in range(len(encoded)): # joins chunks of 15 digits together
+        if i%15 == 0:
+            a = encoded[i:i+15]
+            a = ''.join(a)
+            a = float(a)
+            seq.append(a)
+        
+    return(seq)
+    
+def compare_strains(seq1,seq2): # Compare 2 sequences for total number of mutations
+    counter = 0
+    nums = []
+    for i in range(len(seq1)):
+        if seq1[i] == seq2[i]:
+            pass
+        else:
+            counter += 1
+            nums.append(i+1)
+    
+    return(counter,nums)
+    
+# Entropy function
+import math
+from collections import Counter
+def entropy(labels):
+    num_labels = len(labels)    
+    probability = [count / num_labels for count in Counter(labels).values()]            
+    if num_labels <= 1:
+        return(0)
+    return(sum(-probability * math.log(probability,2))) # this is the formula
